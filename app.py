3cfb4fb378123c01f46c31497917c5f2c2f7499a
msj78598 <166190583+msj78598@users.noreply.github.com>
2025-05-26 05:34:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -356,9 +356,8 @@ if uploaded_file:
         df = df.sort_values(by=sort_col, ascending=asc_bool)
     
     with st.spinner('جاري تحميل النماذج...'):
-        from ultralytics.models.yolo.model import DetectionModel
-        model = DetectionModel("models/best.pt")
-
+        import torch
+        model_yolo = torch.hub.load('ultralytics/yolov5', 'custom', path=MODEL_PATH, force_reload=True)
         model_ml = joblib.load(ML_MODEL_PATH)
         scaler = joblib.load(SCALER_PATH)
     st.success("✅ تم تحميل النماذج بنجاح")
